a0f7dda4d21dfb03a7bf8c1a9e9070c56446dffd
Balgra <andreibalgradean16@gmail.com>
2024-03-26 22:09:06 +0100

    Added Routing Functionality for all Pages

---
+++ frontend/src/Routing_Component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import OrderHistory from '../src/pages/OrderHistory/OrderHistory';
+import CartPage from '../src/pages/Cart/CartPage';
+import CustomizePage from '../src/pages/CustomizeItem/CustomizePage';
+import HomePage from '../src/pages/Home/HomePage';
+import ItemPage from '../src/pages/Item/ItemPage';
+import ItemsPage from '../src/pages/Item/ItemsPage';
+import Login from '../src/pages/Login/LoginPage';
+
+function RoutingComponent() {
+    return (
+        <Router>
+            <Routes>
+            <Route path="" Component={HomePage} />
+            <Route path="/cart" Component={CartPage} />
+            <Route path="/customize" Component={CustomizePage} />
+            <Route path="/item" Component={ItemPage} />
+            <Route path="/items" Component={ItemsPage} />
+            <Route path="/order-history" Component={OrderHistory} />
+            <Route path="/login" Component={Login} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default RoutingComponent;
