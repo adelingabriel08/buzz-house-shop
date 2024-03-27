da4a510532ed4f24c9fcc04eaf31ed276de78c71
Denis Ardelean (Consultant) <Denis.Ardelean-3rdpty@brooksautomation.com>
2024-03-27 14:31:28 +0200

    file and folder organisation

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import './App.css';
-import RoutingComponent from "./Routing_Component";
+import '../../App.css';
+import RoutingComponent from "../../Routing_Component";
 
 function App() {
   return (
     <div className="App">
-      <RoutingComponent />
     </div>
   );
 }
