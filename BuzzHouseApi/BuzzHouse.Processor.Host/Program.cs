4194b429a62e1402d754edc311b537d2e1660ae3
Adelin Chis <adelingabriel08@gmail.com>
2024-04-02 22:21:44 +0300

    configuring cosmos client

--- BuzzHouseApi/BuzzHouse.Processor.Host/Program.cs
+++ BuzzHouseApi/BuzzHouse.Processor.Host/Program.cs
@@ -1,7 +1,15 @@
 using BuzzHouse.Processor.Host;
+using BuzzHouse.Processor.Host.Options;
+using Microsoft.Azure.Cosmos;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
+var cosmosClient = new CosmosClient(builder.Configuration["CosmosCredentials:Endpoint"], builder.Configuration["CosmosCredentials:Key"]);
+
+builder.Services.AddSingleton(cosmosClient);
+builder.Services.AddOptions<ChangeFeedProcessorOptions>()
+    .Bind(builder.Configuration.GetSection(nameof(ChangeFeedProcessorOptions)));
+    
 var host = builder.Build();
 host.Run();
